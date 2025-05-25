fa130fd380cd5c3de1e2c5b88fde67106d53518a
senna-js <bloggernolimit@gmail.com>
2025-05-25 18:54:11 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ dotenv.config()
 const express = require("express")
 const cors = require("cors")
 const drakorkita = require("./src/routes/drakorkita")
-const komiku = require("./src/routes/komiku")
-const kuramanime = require("./src/routes/kuramanime")
 
 const app = express()
 
@@ -15,18 +13,14 @@ app.use(express.json())
 app.use(cors())
 
 app.use("/drakorkita", drakorkita)
-app.use("/komiku", komiku)
-app.use("/kuramanime", kuramanime)
 
 app.get("/", (req, res) => {
     res.status(200).json({
-        message: "Unofficial Komik, Drakor and Anime APIs",
+        message: "Unofficial Drakor APIs",
         developed_by: "ZeroneDoo",
         github: "https://github.com/senna-js/zeronewatch-api.git",
         data: {
-            komiku_url: process.env.KOMIKU_URL,
             drakorkita_url: process.env.DRAKORKITA_URL,
-            kuramanime_url: process.env.KURAMANIME_URL,
         }
     })
 })
